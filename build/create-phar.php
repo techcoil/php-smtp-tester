52cab7afa8b322b997cf5db78cb0f5a4f07aa370
Yaron Uliel <yaron@techmarketing.co.il>
2020-02-05 14:56:24 +0200

    Create build process for phar

---
+++ build/create-phar.php
@@ -0,0 +1,45 @@
+<?php
+// The php.ini setting phar.readonly must be set to 0
+
+if(ini_get('phar.readonly')) {
+    fwrite(STDERR, "PHAR creation failed. php ini setting phar.readonly must bet set to 0\n");
+    exit(1);
+}
+
+chdir(__DIR__);
+
+$file_name = 'php-smtp-tester.phar';
+
+$dir = '../release';
+
+$pharFile = $dir . '/' . $file_name;
+
+// clean up
+if (file_exists($pharFile)) {
+    unlink($pharFile);
+}
+if (file_exists($pharFile . '.gz')) {
+    unlink($pharFile . '.gz');
+}
+
+// create phar
+$p = new Phar($pharFile);
+
+$p->startBuffering();
+
+$default_stub = $p->createDefaultStub('index.php', '/index.php');
+
+// creating our library using whole directory
+$p->buildFromDirectory('../src/');
+
+$stub = "#!/usr/bin/env php\n{$default_stub}";
+
+// plus - compressing it into gzip
+$p->compress(Phar::GZ);
+
+$p->setStub($stub);
+$p->stopBuffering();
+
+
+echo "$file_name successfully created";
+echo "\n";
---
+++ build/install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd $DIR
+
+file_name="php-smtp-tester";
+release_dir="../release";
+
+php -d phar.readonly=0 ./create-phar.php
+
+path="${release_dir}/${file_name}.phar"
+
+if [ -f "$path" ]; then
+  chmod +x "$path"
+  echo "Copying php-smtp-tester to yout PATH"
+  cp ../release/php-smtp-tester.phar /usr/local/bin/php-smtp-tester
+  php-smtp-tester --help
+else
+  echo "Build failed" 1>&2
+fi
+
